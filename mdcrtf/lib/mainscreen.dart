3ee5c97e808decced1de758ef5c0ec4f45eb6bff
Abhishek Rajput <137057456+k9rituraj@users.noreply.github.com>
2024-04-02 17:44:57 +0530

    Update mainscreen.dart

--- mdcrtf/lib/mainscreen.dart
+++ mdcrtf/lib/mainscreen.dart
@@ -5,131 +5,175 @@ import 'roadmap_screen.dart';
 import 'notifications_screen.dart';
 import 'profile_screen.dart';
 
-class MainScreen extends StatefulWidget {
+class HomeIcon extends StatelessWidget {
+  final String activeImageUrl;
+  final String inactiveImageUrl;
+  final bool isActive;
+  final double width;
+  final double height;
+
+  HomeIcon({
+    required this.activeImageUrl,
+    required this.inactiveImageUrl,
+    required this.isActive,
+    required this.width,
+    required this.height,
+  });
+
   @override
-  _MainScreenState createState() => _MainScreenState();
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Image.network(
+        isActive ? activeImageUrl : inactiveImageUrl,
+        width: width,
+        height: height,
+      ),
+    );
+  }
 }
 
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
+class MainScreen extends StatefulWidget {
+  @override
+  _BottomBarScreenState createState() => _BottomBarScreenState();
+}
 
-  // Define your screens here
-  final List<Widget> _screens = [
-    DashboardScreen(),
-    AcademyScreen(),
-    RoadmapScreen(),
-    NotificationsScreen(),
-    ProfileScreen(),
-  ];
+class _BottomBarScreenState extends State<MainScreen> {
+  late List<Map<String, Widget>> _pages;
+  int _selectedPageIndex = 0;
+  bool _isRoadmapScreenActive = false;
+
+  @override
+  void initState() {
+    _pages = [
+      {
+        'page': DashboardScreen(),
+      },
+      {
+        'page': AcademyScreen(),
+      },
+      {
+        'page': RoadmapScreen(),
+      },
+      {
+        'page': NotificationsScreen(),
+      },
+      {
+        'page': ProfileScreen(),
+      },
+    ];
+    super.initState();
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+      _isRoadmapScreenActive = (index == 2);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: _selectedIndex == 0
-                ? Image.network(
-              'https://rtfapi.modicare.com/img/HomeIconSelected@2x.png',
-              height: 24, // Adjust the height as needed
-            )
-                : Image.network(
-              'https://rtfapi.modicare.com/img/Home.png',
-              height: 24, // Adjust the height as needed
-            ),
-            label: '', // Empty label
-          ),
-          BottomNavigationBarItem(
-            icon: _selectedIndex == 1
-                ? Image.network(
-              'https://rtfapi.modicare.com/img/SchoolSelected@2x.png',
-              height: 24, // Adjust the height as needed
-            )
-                : Image.network(
-              'https://rtfapi.modicare.com/img/schoolunselected.png',
-              height: 24, // Adjust the height as needed
-
-            ),
-            label: '', // Empty label
-          ),
-          BottomNavigationBarItem(
-            icon: _selectedIndex == 2
-                ? Image.network(
-              'https://rtfapi.modicare.com/img/RoadMapSelected@2x.png',
-              height: 24, // Adjust the height as needed
-            )
-                : Image.network(
-              'https://rtfapi.modicare.com/img/roadmap.png',
-              height: 24, // Adjust the height as needed
-            ),
-            label: '', // Empty label
-          ),
-          BottomNavigationBarItem(
-            icon: Stack(
-              children: [
-                _selectedIndex == 3
-                    ? Image.network(
-                  'https://rtfapi.modicare.com/img/NotificationSelected@2x.png',
-                  height: 24, // Adjust the height as needed
-                )
-                    : Image.network(
-                  'https://rtfapi.modicare.com/img/Notification.png',
-                  height: 24, // Adjust the height as needed
-                ),
-                Positioned(
-                  right: 0,
-                  child: Container(
-                    padding: EdgeInsets.all(2),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    constraints: BoxConstraints(
-                      minWidth: 16,
-                      minHeight: 16,
-                    ),
-                    child: Text(
-                      '30',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 10,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
+      body: _pages[_selectedPageIndex]['page']!,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white, // Background color
+        shape: CircularNotchedRectangle(),
+        notchMargin: 0.01,
+        clipBehavior: Clip.antiAlias,
+        child: Container(
+          height: kBottomNavigationBarHeight * 0.98,
+          child: Container(
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed, // Fixed type to prevent shifting
+              onTap: _selectPage,
+              backgroundColor: Colors.white,
+              unselectedItemColor: Theme.of(context).textTheme.bodyText1!.color,
+              selectedItemColor: Colors.blue,
+              currentIndex: _selectedPageIndex,
+              items: [
+                BottomNavigationBarItem(
+                  icon: HomeIcon(
+                    activeImageUrl:
+                    'https://rtfapi.modicare.com/img/HomeIconSelected@2x.png',
+                    inactiveImageUrl:
+                    'https://rtfapi.modicare.com/img/Home.png',
+                    isActive: _selectedPageIndex == 0,
+                    width: 24, // Set the desired width
+                    height: 24, // Set the desired height
                   ),
+                  label: '', // No label
+                ),
+                BottomNavigationBarItem(
+                  icon: HomeIcon(
+                    activeImageUrl:
+                    'https://rtfapi.modicare.com/img/SchoolSelected@2x.png',
+                    inactiveImageUrl:
+                    'https://rtfapi.modicare.com/img/schoolunselected.png',
+                    isActive: _selectedPageIndex == 1,
+                    width: 24, // Set the desired width
+                    height: 24, // Set the desired height
+                  ),
+                  label: '', // No label
+                ),
+                BottomNavigationBarItem(
+                  // Use appropriate icon, in this case, no icon is used
+                  icon: Icon(null),
+                  label: '', // No label
+                ),
+                BottomNavigationBarItem(
+                  icon: HomeIcon(
+                    activeImageUrl:
+                    'https://rtfapi.modicare.com/img/NotificationSelected@2x.png',
+                    inactiveImageUrl:
+                    'https://rtfapi.modicare.com/img/Notification.png',
+                    isActive: _selectedPageIndex == 3,
+                    width: 24, // Set the desired width
+                    height: 24, // Set the desired height
+                  ),
+                  label: '', // No label
+                ),
+                BottomNavigationBarItem(
+                  icon: HomeIcon(
+                    activeImageUrl:
+                    'https://rtfapi.modicare.com/img/ProfileSelected@2x.png',
+                    inactiveImageUrl:
+                    'https://rtfapi.modicare.com/img/profile.png',
+                    isActive: _selectedPageIndex == 4,
+                    width: 24, // Set the desired width
+                    height: 24, // Set the desired height
+                  ),
+                  label: '', // No label
                 ),
               ],
             ),
-            label: '', // Empty label
           ),
-          BottomNavigationBarItem(
-            icon: _selectedIndex == 4
-                ? Image.network(
-              'https://rtfapi.modicare.com/img/ProfileSelected@2x.png',
-              height: 24, // Adjust the height as needed
-            )
-                : Image.network(
-              'https://rtfapi.modicare.com/img/profile.png',
-              height: 24, // Adjust the height as needed
-            ),
-            label: '', // Empty label
+        ),
+      ),
+      floatingActionButtonLocation:
+      FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FloatingActionButton(
+          hoverElevation: 22,
+          backgroundColor: Colors.white,
+          splashColor: Colors.transparent, // Remove splash color
+          tooltip: 'Search',
+          elevation: 4,
+          child: Image.network(
+            _isRoadmapScreenActive
+                 ? 'https://rtfapi.modicare.com/img/RoadMapSelected@2x.png'
+                : 'https://rtfapi.modicare.com/img/roadmap.png',
+            width: 24, // Set the desired width
+            height: 24, // Set the desired height
           ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color(0xFF535353),
-        unselectedItemColor: Color(0xFF535353),
-        onTap: _onItemTapped,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
+          onPressed: () => setState(() {
+            _selectedPageIndex = 2;
+            _isRoadmapScreenActive = true; // Update the variable when pressing the floating button
+          }),
+        ),
       ),
     );
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 }
